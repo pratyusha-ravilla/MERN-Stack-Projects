9d2f7b7b24ee118b12e57ae90c57daef89b1ed0c
pratyusha-ravilla <pratyusharavilla123@gmail.com>
2024-01-03 21:37:41 +0530

    redux

---
+++ Redux/src/app/store.js
@@ -0,0 +1,9 @@
+//importing from redux toolkit
+import {configureStore} from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
+
+
+export const store = configureStore({
+  reducer: todoReducer,
+  
+})
